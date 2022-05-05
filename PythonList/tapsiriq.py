4a76da24f49f56852a5f441a8d229076cf223cc9
Gunduz-Deddi <deyzi712@mail.ru>
2022-05-06 01:25:26 +0400

    tapsiriq dictionary

--- PythonList/tapsiriq.py
+++ PythonList/tapsiriq.py
@@ -1,5 +1,14 @@
-import datetime
-d=datetime.datetime.now()
-print(d)
-
+#import datetime
+#d=datetime.datetime.now()
+#print(d)
+a="""[
+    {"Komanda":"Liverpool",
+    "Missiya":"Qelebe",
+    "NOvbeti oyun":"07.05.2022",
+    },{"klub":"Mancester city",
+    "premyer liga":"1-ci yerde",
+    "Cempion":"ingiltere premyer ligasi"}
+    ]
+"""
+print(a[{"Komanda"}])
 
